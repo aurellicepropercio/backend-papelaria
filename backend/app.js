a5dd9a5943b1cf28fcc50dd7def185ffc8950fe6
aurellicepropercio <aurelice-vida@hotmail.com>
2024-05-16 11:19:09 -0300

    instalei o nodemon

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const app = express();
-
-app.use((req, res, next)=>{
-    res.send("minha api")
+const usuario=[
+    {nome:'carlos', idade:'20', peso:'100'},
+    {nome:'pedro', idade:'30'},
+    {nome:'joao', idade:'18', peso: '75', cep: '77807060'}
+]
+app.use("/todos",(req, res, next)=>{
+    res.send(usuario)
 })
 
+app.use("/nome",(req, res, next)=>{
+    res.send(usuario[1].nome)
+})
+app.use("/cep",(req, res, next)=>{
+    res.send(cep)
+})
+
+app.use("/soma",(req, res, next)=>{ total=0;
+    let toal=0;
+    for(i=0;  i<usuario.length;  i++)
+    {
+     total=total+parseInt(usuario[i].idade)
+    }
+    res.send("Soma total: "+total);
+})
 module.exports = app
